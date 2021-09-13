5033c422d3ac3cd6824b07347a3253cf0bf35fc3
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-09-13 17:41:35 +0200

    Dinamic messages

--- js/main.js
+++ js/main.js
@@ -93,11 +93,14 @@ const app = new Vue(
                 },
             ],
             selected_user: 0,
+            messages_array: '',
         },
         methods: {
             selectedContact(index) {
                console.log(index);
                this.selected_user = index;
+               this.messages_array = this.contacts[this.selected_user].messages;
+               console.log(this.messages_array)
             }
         }
     }
